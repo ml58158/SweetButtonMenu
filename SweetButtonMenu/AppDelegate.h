5dde5050618f2aff0671ba39342145a5a63f7663
Matt Larkin <mattlarkin@me.com>
2015-03-19 12:09:48 -0400

    Initial Commit

---
+++ SweetButtonMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SweetButtonMenu
+//
+//  Created by Matt Larkin on 3/19/15.
+//  Copyright (c) 2015 Matt Larkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
